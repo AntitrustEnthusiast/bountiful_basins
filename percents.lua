d0eef6d4fa49e97591c3f9b0af6c63998efd3fda
AntitrustEnthusiast <email@email.com>
2025-07-02 00:07:29 -0700

    add per world info to percents script

--- percents.lua
+++ percents.lua
@@ -3,14 +3,24 @@
 -- outputs as CSV, pipe to something like ConvertFrom-CSV to print as a nice table:
 require("materials")
 
+-- pass as first arg:
+--   lua percents.lua 75
+local fill_percent = (arg[1] or 25) / 100
+
+-- number of replaced basins per parallel world
+local basin_count = 52
+local filled_per_world = basin_count * fill_percent
+
 local total_weight = 0
 for _, value in pairs(DEFAULT_WEIGHTS) do
     total_weight = total_weight + value
 end
 
-print("name,weight,percent")
+print("name,weight,percent_basin,percent_world")
 for key, value in pairs(DEFAULT_WEIGHTS) do
     local escaped = key:gsub('"', '""')
-    local percent = (value / total_weight) * 100
-    print('"' .. escaped .. '",' .. value .. ',' .. percent)
+    local probability = (value / total_weight)
+    local percent = probability * 100
+    local per_world = (1 - ((1 - probability)^filled_per_world)) * 100
+    print('"' .. escaped .. '",' .. value .. ',' .. percent .. ',' .. per_world)
 end
